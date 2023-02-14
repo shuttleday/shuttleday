cb9ab64059892f51c318f233ceb60ac2655bc5e3
PScoriae <pierreccesario@gmail.com>
2023-02-14 15:12:57 +0800

    chore: initialise ts

--- bdlist-backend/index.ts
+++ bdlist-backend/index.ts
@@ -1,12 +1,8 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express'
 const app = express();
 
 // Parse requests of content-type - application/json.
 app.use(express.json());
-
-// Add CORS suport.
-app.use(cors());
 
 // Simple Hello World route.
 app.get('/', (req, res) => {
@@ -14,7 +10,7 @@ app.get('/', (req, res) => {
 });
 
 // Set port, listen for requests.
-const PORT = 4000;
+const PORT = 2600;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
