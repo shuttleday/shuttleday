3ce9618e1f264b33cf32e72a368419daf75a5d74
PScoriae <pierreccesario@gmail.com>
2023-02-20 19:37:40 +0800

    feat: add get users endpoint

--- bdlist-backend/src/routes/users.ts
+++ bdlist-backend/src/routes/users.ts
@@ -27,25 +27,37 @@ router.get("/:email", async (req: Request, res: Response) => {
   res.status(200).json(user);
 });
 
-// Create new user
-router.post("/", checkDupeUser, async (req: Request, res: Response) => {
-  try {
-    const document: User = {
-      _id: new ObjectId(),
-      email: req.body.email,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      username: req.body.username,
-      createdAt: new Date(),
-      userType: "player",
-    };
+router
+  .route("/")
+  .get(async (req: Request, res: Response) => {
+    try {
+      const result = await Users.find().toArray();
 
-    const result = await Users.insertOne(document);
-    res.status(200).json({ result, document });
-  } catch (error) {
-    log.error(error);
-    res.sendStatus(500);
-  }
-});
+      res.status(200).json({ result });
+    } catch (error) {
+      log.error(error);
+      res.sendStatus(500);
+    }
+  })
+  // Create new user
+  .post(checkDupeUser, async (req: Request, res: Response) => {
+    try {
+      const document: User = {
+        _id: new ObjectId(),
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        username: req.body.username,
+        createdAt: new Date(),
+        userType: "player",
+      };
+
+      const result = await Users.insertOne(document);
+      res.status(200).json({ result, document });
+    } catch (error) {
+      log.error(error);
+      res.sendStatus(500);
+    }
+  });
 
 export default router;
