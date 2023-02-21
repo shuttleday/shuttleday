a1b4de650cc5f17abc14684ea44ddf287d0281fa
PScoriae <pierreccesario@gmail.com>
2023-02-21 19:26:23 +0800

    fix: specify db

--- bdlist-backend/src/db/collections.ts
+++ bdlist-backend/src/db/collections.ts
@@ -1,7 +1,7 @@
 import { client } from "./connect";
 import { User, GameSession } from "./interfaces";
 
-const db = client.db();
+const db = client.db("shuttleday-prod");
 
 export const Users = db.collection<User>("users");
 export const GameSessions = db.collection<GameSession>("game-sessions");
