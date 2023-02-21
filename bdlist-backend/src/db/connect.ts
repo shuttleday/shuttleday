24a7ca660056a162e14100ef395b91876e563fa3
PScoriae <pierreccesario@gmail.com>
2023-02-21 15:16:20 +0800

    feat: add mongodb auth

--- bdlist-backend/src/db/connect.ts
+++ bdlist-backend/src/db/connect.ts
@@ -3,17 +3,22 @@ import log from "../utils/logger";
 import * as dotenv from "dotenv";
 dotenv.config();
 
+const username = encodeURIComponent(process.env.MONGO_USER!);
+const password = encodeURIComponent(process.env.MONGO_PW!);
 const dbUri = process.env.MONGO_URL as string;
-export const client = new MongoClient(dbUri, {
+const uri = `mongodb://${username}:${password}@${dbUri}`;
+export const client = new MongoClient(uri, {
   minPoolSize: 10,
 });
 
 async function connect() {
   try {
+    log.info(`Attempting connection to: ${uri}`);
     await client.connect();
     log.info("Database connected");
   } catch (error) {
-    log.error("Failed to connect to db", error);
+    log.error("Failed to connect to db");
+    log.error(error);
     process.exit(1);
   }
 }
