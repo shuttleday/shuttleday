343a0b6cede1f774a76487a899879dc0b29c9e3c
PScoriae <pierreccesario@gmail.com>
2023-02-22 22:57:13 +0800

    perf: merge two db calls

--- bdlist-backend/src/routes/user-payments.ts
+++ bdlist-backend/src/routes/user-payments.ts
@@ -11,7 +11,7 @@ import {
 
 const router = Router();
 
-// Update user payment status
+// Upload payment receipt and update payment status
 router.post(
   "/",
   fileUpload(),
@@ -19,32 +19,11 @@ router.post(
   fileSizeLimiter,
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      // Verify sessionId
-      const gameSession = await GameSessions.findOne({
-        _id: new ObjectId(req.body.sessionId),
-        "players.userEmail": req.user.email,
-      });
-
-      if (!gameSession)
-        return res
-          .status(400)
-          .json({ error: "No session with that id and your email" });
-
-      // Upload files to S3
-      const file = processUploadedFiles(req.files!);
-      const bucketParams = {
-        Bucket: process.env.AWS_S3_BUCKET_NAME!,
-        Key: `${gameSession.start.toUTCString()}-${req.user.username}.${
-          file.name.split(".")[1]
-        }`,
-        Body: file.data,
-      };
-      const data = await s3.send(new PutObjectCommand(bucketParams));
-
-      const result = await GameSessions.updateOne(
+      const result = await GameSessions.findOneAndUpdate(
         {
           _id: new ObjectId(req.body.sessionId),
-          "players.userEmail": req.user.email,
+          "players.userEmail": { $in: [req.user.email] },
+          "players.paid": { $eq: false },
           "players.paidAt": { $eq: null }, // prevents overwrites if already paid
         },
         {
@@ -52,10 +31,33 @@ router.post(
             "players.$.paid": true,
             "players.$.paidAt": new Date(),
           },
-        }
+        },
+        { returnDocument: "after" }
       );
 
-      res.status(201).json({ result });
+      if (result.value === null)
+        return res.status(409).json({
+          error: "Either already paid for this session or not in session",
+        });
+
+      const modified = result.value.players?.find(
+        (user) => user.userEmail === req.user.email
+      );
+
+      // Upload files to S3
+      const file = processUploadedFiles(req.files!);
+      const filename = `${result.value.start.toUTCString()}-${
+        req.user.username
+      }.${file.name.split(".")[1]}`;
+
+      const bucketParams = {
+        Bucket: process.env.AWS_S3_BUCKET_NAME!,
+        Key: filename,
+        Body: file.data,
+      };
+      const data = await s3.send(new PutObjectCommand(bucketParams));
+
+      res.status(201).json({ result: modified });
     } catch (error) {
       next(error);
     }
