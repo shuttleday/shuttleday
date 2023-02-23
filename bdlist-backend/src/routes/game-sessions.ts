d893ba0dbbb3040ebcef6893dc44b5c9254793ef
PScoriae <pierreccesario@gmail.com>
2023-02-23 22:13:59 +0800

    fix: use user from token

--- bdlist-backend/src/routes/game-sessions.ts
+++ bdlist-backend/src/routes/game-sessions.ts
@@ -44,7 +44,7 @@ router
         courts: req.body.courts,
         group: req.body.group,
         createdAt: new Date(),
-        payTo: req.body.payToUser,
+        payTo: req.user!.email,
       };
 
       try {
