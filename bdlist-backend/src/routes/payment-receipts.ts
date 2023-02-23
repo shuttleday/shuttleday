ba762832169cdea77c95183472bd107d63544724
PScoriae <pierreccesario@gmail.com>
2023-02-23 22:43:43 +0800

    perf: get urls asynchronously

--- bdlist-backend/src/routes/payment-receipts.ts
+++ bdlist-backend/src/routes/payment-receipts.ts
@@ -11,6 +11,8 @@ router.get(
   adminCheck,
   async (req: Request, res: Response, next: NextFunction) => {
     try {
+      const { fromDate, toDate } = validateDates(req);
+
       // Fetch first 1000 objects in S3
       const bucketParams = { Bucket: process.env.AWS_S3_BUCKET_NAME };
       const getListObjects = async () => {
@@ -20,24 +22,34 @@ router.get(
       const objects = await getListObjects();
 
       // Filter ListObjects by date
-      const { fromDate, toDate } = validateDates(req);
-
       const filteredObjects = objects?.filter((obj) => {
         return fromDate <= obj.LastModified! && obj.LastModified! <= toDate;
       });
 
-      // Get signed URLs for public view
-      const signedUrls = [];
+      // Exit early if no match
+      if (!filteredObjects) return res.status(200).json({ signedUrls: [] });
 
-      for (const obj of filteredObjects!) {
+      // Construct promises
+      const promises = filteredObjects.map((obj) => {
         const command = new GetObjectCommand({
           Bucket: process.env.AWS_S3_BUCKET_NAME,
           Key: obj.Key,
         });
-        const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
-        signedUrls.push({ key: obj.Key, signedUrl: url });
-      }
-      res.status(200).json({ signedUrls });
+        return getSignedUrl(s3, command, { expiresIn: 3600 });
+      });
+
+      // Get urls asynchronously
+      const urls = await Promise.all(promises);
+
+      // Parse key names from URLs
+      const urlObjs = urls.map((signedUrl) => {
+        const filename = decodeURIComponent(new URL(signedUrl).pathname).slice(
+          1
+        );
+        return { key: filename, signedUrl };
+      });
+
+      res.status(200).json({ signedUrls: urlObjs });
     } catch (err) {
       next(err);
     }
