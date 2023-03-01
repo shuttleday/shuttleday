101817c48e86a349f5ed6750fcf5f58bb7681264
Jonathan <tehyeuhaw@gmail.com>
2023-03-01 19:41:05 +0800

    chore: refactored speed dial

--- bdlist-frontend/src/pages/Details.jsx
+++ bdlist-frontend/src/pages/Details.jsx
@@ -6,23 +6,16 @@ import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
-import SpeedDial from '@mui/material/SpeedDial';
-import SpeedDialIcon from '@mui/material/SpeedDialIcon';
-import SpeedDialAction from '@mui/material/SpeedDialAction';
-import HomeIcon from '@mui/icons-material/Home';
 import { useNavigate } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import { getUsers } from '../data/repository';
 import { useState } from 'react';
 
+import SpeedDialComponent from '../components/SpeedDialComponent';
+
 const Details = () => {
   let navigate = useNavigate();
 
-  const actions = [{ icon: <HomeIcon />, name: 'Home' }];
-
-  function handleSpeedDial(operation) {
-    navigate('/');
-  }
   const [users, setUsers] = useState(null);
 
   useEffect(() => {
@@ -82,31 +75,7 @@ const Details = () => {
           )}
         </Box>
       </Stack>
-      <Box
-        sx={{
-          transform: 'translateZ(0px)',
-          flexGrow: 1,
-          position: 'fixed',
-          bottom: 0,
-          width: 1.0,
-        }}
-      >
-        <SpeedDial
-          sticky='true'
-          ariaLabel='SpeedDial basic'
-          sx={{ position: 'absolute', bottom: 16, right: 16 }}
-          icon={<SpeedDialIcon />}
-        >
-          {actions.map((action) => (
-            <SpeedDialAction
-              key={action.name}
-              icon={action.icon}
-              tooltipTitle={action.name}
-              onClick={handleSpeedDial}
-            />
-          ))}
-        </SpeedDial>
-      </Box>
+      <SpeedDialComponent />
     </div>
   );
 };
