9e83b245cc70a9344c0b8b18c61768c6b42ceb7e
PScoriae <pierreccesario@gmail.com>
2023-03-18 22:18:54 +0800

    feat: log to file only

--- bdlist-backend/src/utils/logger.ts
+++ bdlist-backend/src/utils/logger.ts
@@ -14,30 +14,7 @@ const prod = {
   ],
 };
 
-const dev = {
-  targets: [
-    {
-      level: "info",
-      target: "pino-pretty",
-      options: {
-        translateTime: "yyyy-mm-dd HH:MM:ss.l",
-        colorize: true,
-        ignore: "pid,hostname",
-      },
-    },
-    {
-      level: "info",
-      target: "pino-pretty",
-      options: {
-        destination: "./last.log",
-        translateTime: "yyyy-mm-dd HH:MM:ss.l",
-        colorize: false,
-      },
-    },
-  ],
-};
-
-const pinoConfig = process.env.NODE_ENV === "production" ? prod : dev;
+const pinoConfig = prod;
 const transport = pino.transport(pinoConfig);
 const log = pino(transport);
 
