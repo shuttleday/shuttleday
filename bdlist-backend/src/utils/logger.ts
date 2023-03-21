474caf5fb74bc456a0db4a2fd669a651fd3e9daf
PScoriae <pierreccesario@gmail.com>
2023-03-21 18:36:40 +0800

    feat: add dev logging

--- bdlist-backend/src/utils/logger.ts
+++ bdlist-backend/src/utils/logger.ts
@@ -14,7 +14,30 @@ const prod = {
   ],
 };
 
-const pinoConfig = prod;
+const dev = {
+  targets: [
+    {
+      level: "info",
+      target: "pino-pretty",
+      options: {
+        translateTime: "yyyy-mm-dd HH:MM:ss.l",
+        colorize: true,
+        ignore: "pid,hostname",
+      },
+    },
+    {
+      level: "info",
+      target: "pino-pretty",
+      options: {
+        destination: "./last.log",
+        translateTime: "yyyy-mm-dd HH:MM:ss.l",
+        colorize: false,
+      },
+    },
+  ],
+};
+
+const pinoConfig = process.env.NODE_ENV === "production" ? prod : dev;
 const transport = pino.transport(pinoConfig);
 const log = pino(transport);
 
