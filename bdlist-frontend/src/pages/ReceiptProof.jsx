8bc7f7dcfc70a208e08d45ff4a90c1f3b118523f
Jonathan <tehyeuhaw@gmail.com>
2023-04-11 01:34:29 +1000

    fix: improved loading and null data display

--- bdlist-frontend/src/pages/ReceiptProof.jsx
+++ bdlist-frontend/src/pages/ReceiptProof.jsx
@@ -12,8 +12,8 @@ import { Box, Stack, Typography } from '@mui/material';
 import { getSession, getReceipts } from '../data/repository';
 
 const ReceiptProof = () => {
-  const [images, setImages] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [images, setImages] = useState(null);
+  const [isNull, setIsNull] = useState(false);
   const [selectedImages, setSelectedImages] = useState(null);
 
   const [currentImage, setCurrentImage] = useState(0);
@@ -37,12 +37,11 @@ const ReceiptProof = () => {
     async function getData() {
       getSession().then((resData) => {
         if (resData.gameSessions !== null) {
-          getReceipts(resData.gameSessions).then((res) => {
-            if (res !== null) {
+          getReceipts(resData.gameSessions)
+            .then((res) => {
               setImages(res);
-            } else {
-            }
-          });
+            })
+            .error(setIsNull(true));
         } else {
           alert('No data found');
         }
@@ -50,90 +49,90 @@ const ReceiptProof = () => {
     }
 
     getData();
-    setIsLoading(false);
     // eslint-disable-next-line
   }, []);
 
+  if (isNull) {
+    return (
+      <Error
+        title={'No data found...'}
+        subTitle={
+          'Either no sessions has been created or no one has paid yet....'
+        }
+      />
+    );
+  } else if (images === null) {
+    return (
+      <Stack
+        spacing={2}
+        display='flex'
+        justifyContent='center'
+        alignItems='center'
+      >
+        <CircularProgress color='success' />
+      </Stack>
+    );
+  }
   return (
     <div>
-      {isLoading ? (
-        <Stack
-          spacing={2}
-          display='flex'
-          justifyContent='center'
-          alignItems='center'
-        >
-          <CircularProgress color='success' />
-        </Stack>
-      ) : images.length > 0 ? (
-        <Stack
-          spacing={2}
-          display='flex'
-          justifyContent='center'
-          alignItems='center'
-        >
-          <Typography
-            id='modal-modal-title'
-            variant='h6'
-            component='h2'
-          ></Typography>
-          {images === [] ? (
-            <Box></Box>
-          ) : (
-            images.map((imageInfo, infoIndex) => (
-              <Box key={infoIndex}>
-                <ImageList
-                  sx={{
-                    width: { sx: 240, sm: 300, md: 500 },
-                    height: { sx: 100, sm: 100, md: 550 },
-                  }}
-                >
-                  <ImageListItem key='Subheader' cols={2}>
-                    <ListSubheader
-                      component='div'
-                      className='bg-primary p-3 rounded-md sm:'
+      <Stack
+        spacing={2}
+        display='flex'
+        justifyContent='center'
+        alignItems='center'
+      >
+        <Typography
+          id='modal-modal-title'
+          variant='h6'
+          component='h2'
+        ></Typography>
+        {images === [] ? (
+          <Box></Box>
+        ) : (
+          images.map((imageInfo, infoIndex) => (
+            <Box key={infoIndex}>
+              <ImageList
+                sx={{
+                  width: { sx: 240, sm: 300, md: 500 },
+                  height: { sx: 100, sm: 100, md: 550 },
+                }}
+              >
+                <ImageListItem key='Subheader' cols={2}>
+                  <ListSubheader
+                    component='div'
+                    className='bg-primary p-3 rounded-md sm:'
+                  >
+                    <Typography
+                      id='modal-modal-title'
+                      className='text-white font-medium lg:text-[28px] sm:text-[25px] xs:text-[17px] text-[16px] lg:leading-[40px] text-center'
                     >
-                      <Typography
-                        id='modal-modal-title'
-                        className='text-white font-medium lg:text-[28px] sm:text-[25px] xs:text-[17px] text-[16px] lg:leading-[40px] text-center'
-                      >
-                        {dayjs(imageInfo.date).format(
-                          'DD/MM/YYYY dddd hh:mm A'
-                        )}
-                      </Typography>
-                    </ListSubheader>
-                  </ImageListItem>
+                      {dayjs(imageInfo.date).format('DD/MM/YYYY dddd hh:mm A')}
+                    </Typography>
+                  </ListSubheader>
+                </ImageListItem>
 
-                  {imageInfo.urls.map((image, index) => (
-                    <ImageListItem key={index}>
-                      <img
-                        className='rounded-md'
-                        src={`${image.signedUrl}`}
-                        srcSet={`${image.signedUrl}`}
-                        alt={image.payer}
-                        onClick={() => openImageViewer(index, infoIndex)}
-                      />
-                      <ImageListItemBar
-                        title={image.payer}
-                        subtitle={''}
-                        className='rounded-md'
-                      />
-                    </ImageListItem>
-                  ))}
-                </ImageList>
-                <br />
-              </Box>
-            ))
-          )}
-        </Stack>
-      ) : (
-        <Error
-          title={'No data found...'}
-          subTitle={
-            'Either no sessions has been created or no one has paid yet....'
-          }
-        />
-      )}
+                {imageInfo.urls.map((image, index) => (
+                  <ImageListItem key={index}>
+                    <img
+                      className='rounded-md'
+                      src={`${image.signedUrl}`}
+                      srcSet={`${image.signedUrl}`}
+                      alt={image.payer}
+                      onClick={() => openImageViewer(index, infoIndex)}
+                    />
+                    <ImageListItemBar
+                      title={image.payer}
+                      subtitle={''}
+                      className='rounded-md'
+                    />
+                  </ImageListItem>
+                ))}
+              </ImageList>
+              <br />
+            </Box>
+          ))
+        )}
+      </Stack>
 
       {isViewerOpen && (
         <ImageViewer
