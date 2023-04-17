70e4bdbbb6386e4cb00c707f2e4a6f54f8ff1d22
PScoriae <pierreccesario@gmail.com>
2023-04-17 18:01:15 +0800

    feat: add post /admins/qr

---
+++ bdlist-backend/src/routes/admins.ts
@@ -0,0 +1,82 @@
+import { NextFunction, Request, Response, Router } from "express";
+import { ApiError } from "../utils/error-util";
+import {
+  adminCheck,
+  fileSizeLimiter,
+  validateFileUpload,
+} from "../middleware/validateRequest";
+import fileUpload from "express-fileupload";
+import { s3, processUploadedFiles } from "../utils/functions";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { Users } from "../db/collections";
+
+const router = Router();
+// /admins
+
+router
+  .route("/qr")
+  // Get QR code by email
+  .get(adminCheck, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      res.status(200).json();
+      next();
+    } catch (error) {
+      next(error);
+    }
+  })
+  // Create QR code
+  .post(
+    adminCheck,
+    fileUpload(),
+    validateFileUpload,
+    fileSizeLimiter,
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const result = await Users.findOne({ email: req.user.email });
+        if (!result) throw new ApiError(404, "No user with that email");
+
+        if (result.hasQR)
+          throw new ApiError(409, "You have already uploaded a QR");
+
+        const file = processUploadedFiles(req.files!);
+        const filename = `${req.user.email}-QR.jpg`;
+
+        const bucketParams = {
+          Bucket: process.env.AWS_S3_BUCKET_NAME!,
+          Key: filename,
+          Body: file.data,
+        };
+
+        const data = await s3.send(new PutObjectCommand(bucketParams));
+
+        if (data.$metadata.httpStatusCode !== 200)
+          throw new Error("Could not upload to S3");
+
+        const result2 = await Users.findOneAndUpdate(
+          { email: req.user.email },
+          { $set: { hasQR: true } },
+          { returnDocument: "after" }
+        );
+
+        if (!result2.ok) throw new ApiError(500, "Internal server error");
+
+        res.status(201).json(result2.value);
+        next();
+      } catch (error) {
+        next(error);
+      }
+    }
+  )
+  // Update QR code
+  .patch(
+    adminCheck,
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        next();
+      } catch (error) {
+        next(error);
+      }
+    }
+  );
+
+export default router;
