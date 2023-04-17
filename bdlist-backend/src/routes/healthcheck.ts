9f3d9e05210b4efd5723b79ddf433ddc766ccd28
PScoriae <pierreccesario@gmail.com>
2023-04-18 01:05:38 +0800

    feat: healthcheck now also shows version

--- bdlist-backend/src/routes/healthcheck.ts
+++ bdlist-backend/src/routes/healthcheck.ts
@@ -1,11 +1,11 @@
 import { Router, Request, Response, NextFunction } from "express";
+import version from "../version.json";
 
 const router = Router();
 
 // Healthcheck for API service
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
-  // Must be sendStatus or else it breaks the route.
-  res.sendStatus(200);
+  res.status(200).json(version);
   next();
 });
 
