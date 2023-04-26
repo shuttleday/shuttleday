220f0c176e9344ab3b66828adcee0828c597063e
PScoriae <pierreccesario@gmail.com>
2023-04-27 00:50:19 +0800

    chore: update code comments

--- bdlist-backend/src/index.ts
+++ bdlist-backend/src/index.ts
@@ -4,7 +4,7 @@ import { connectDb } from "./db/connect.js";
 
 const port = process.env.SERVER_PORT as string;
 
-// Start server and connect to db
+// Start server and connect to MongoDB
 app.listen(parseInt(port), async () => {
   log.info(`Server is running on port ${port}.`);
   await connectDb();
