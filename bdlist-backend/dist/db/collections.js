ff59895c192044978867fcc965405035fb32a71a
PScoriae <pierreccesario@gmail.com>
2023-04-26 15:57:28 +0800

    chore: remove dist from commits

--- bdlist-backend/dist/db/collections.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GameSessions = exports.Users = void 0;
-const connect_1 = require("./connect");
-const db = connect_1.client.db("shuttleday-prod");
-exports.Users = db.collection("users");
-exports.GameSessions = db.collection("game-sessions");
-//# sourceMappingURL=collections.js.map
