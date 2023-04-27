f5babbb588e6147af20e367d88349e8af462e694
PScoriae <pierreccesario@gmail.com>
2023-04-27 12:20:41 +0800

    feat: add fileExt to file upload routes
    used to keep state of the file's extension in upload routes.
    makes it easy to determine what to name the file in S3

--- bdlist-backend/src/@types/express/index.d.ts
+++ bdlist-backend/src/@types/express/index.d.ts
@@ -1,5 +1,7 @@
 import { Request } from "express";
 import { User } from "../../db/interfaces.js";
+import { FileExtension } from "file-type";
+import fileUpload from "express-fileupload";
 
 // to make the file a module and avoid the TypeScript error
 export {};
@@ -8,6 +10,7 @@ declare global {
   namespace Express {
     export interface Request {
       user: User;
+      fileExt?: FileExtension;
     }
   }
 }
