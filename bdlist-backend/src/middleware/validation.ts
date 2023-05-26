557470b4aad2366327362fa69a2e724ec4b899ab
PScoriae <pierreccesario@gmail.com>
2023-05-26 21:34:23 +0800

    chore: update validation

--- bdlist-backend/src/middleware/validation.ts
+++ bdlist-backend/src/middleware/validation.ts
@@ -4,17 +4,22 @@ export interface requiredMETHOD {
 
 export const requiredPOST: requiredMETHOD = {
   "/auth/register": ["username"],
-  "/game-sessions": ["start", "end", "cost", "courts"],
+  "/rooms/:roomId/sessions": ["start", "end", "cost", "courts"],
   "/session-players": ["sessionId"],
   "/refreshToken": ["refreshToken"],
   "/rooms": ["name"],
 };
 
-export const requiredGET: requiredMETHOD = {
-  "/payment-receipts": ["sessionId"],
-};
+export const requiredGET: requiredMETHOD = {};
 
 export const requiredPATCH: requiredMETHOD = {
-  "/game-sessions": ["sessionId", "start", "end", "cost", "payTo", "courts"],
+  "/rooms/:roomId/sessions": [
+    "sessionId",
+    "start",
+    "end",
+    "cost",
+    "payTo",
+    "courts",
+  ],
   "/rooms": ["name"],
 };
