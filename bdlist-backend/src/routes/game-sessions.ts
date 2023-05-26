593c54ebf534ac438f8bfce5f07b6efbcdb1a266
PScoriae <pierreccesario@gmail.com>
2023-05-26 21:03:41 +0800

    fix: correct game session logic

--- bdlist-backend/src/routes/game-sessions.ts
+++ bdlist-backend/src/routes/game-sessions.ts
@@ -29,8 +29,7 @@ router
 
       const gameSessions = await GameSessions.find(query).toArray();
 
-      if (gameSessions.length === 0)
-        throw new ApiError(404, "No sessions in that room ID");
+      if (gameSessions.length === 0) throw new ApiError(404, "No sessions");
 
       res.status(200).json({ gameSessions });
       next();
@@ -81,61 +80,12 @@ router
         next(error);
       }
     }
-  )
-  .patch(async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      // Validate courts key
-      if (!(req.body.courts instanceof Array))
-        throw new ApiError(400, "courts key must be of type string[]");
-      for (const elem of req.body.courts) {
-        if (typeof elem !== "string")
-          throw new ApiError(400, "courts key must be of type string[]");
-      }
-
-      if (typeof req.body.cost !== "number")
-        throw new ApiError(400, "cost key must be of type number");
-
-      // check if requester is an admin of the room by roomId
-      const userEmail = req.user.email;
-      const roomId = req.params.roomId;
-      if (!isValidObjectId(roomId))
-        throw new ApiError(400, "Not a valid room ID");
-
-      const room = await Rooms.findOne({ _id: new ObjectId(roomId) });
-      if (!room) throw new ApiError(404, "No room with that ID");
-
-      const roomAdminList = room.adminList;
-      if (!roomAdminList.includes(userEmail))
-        throw new ApiError(403, "You are not an admin of this room");
-
-      // Update and return new document
-      const result = await GameSessions.findOneAndUpdate(
-        { _id: new ObjectId(req.body.sessionId) },
-        {
-          $set: {
-            start: new Date(req.body.start),
-            end: new Date(req.body.end),
-            cost: req.body.cost as number,
-            payTo: req.body.payTo,
-            courts: req.body.courts as string[],
-            title: req.body.title,
-          },
-        },
-        { returnDocument: "after" }
-      );
-      if (result.value === null)
-        throw new ApiError(404, "No session with that id");
-
-      res.status(200).json({ result: result.value });
-    } catch (error) {
-      next(error);
-    }
-  });
+  );
 
 // Get session by id
-router.get(
-  "/sessions/:sessionId",
-  async (req: Request, res: Response, next: NextFunction) => {
+router
+  .route("/sessions/:sessionId")
+  .get(async (req: Request, res: Response, next: NextFunction) => {
     try {
       // ID validation
       const sessionId = req.params.sessionId;
@@ -161,7 +111,63 @@ router.get(
     } catch (error) {
       next(error);
     }
-  }
-);
+  })
+  .patch(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      // Validate courts key
+      if (!(req.body.courts instanceof Array))
+        throw new ApiError(400, "courts key must be of type string[]");
+      for (const elem of req.body.courts) {
+        if (typeof elem !== "string")
+          throw new ApiError(400, "courts key must be of type string[]");
+      }
+
+      if (typeof req.body.cost !== "number")
+        throw new ApiError(400, "cost key must be of type number");
+
+      // Session ID validation
+      const sessionId = req.params.sessionId;
+      if (!isValidObjectId(sessionId))
+        throw new ApiError(400, "Not a valid session ID");
+
+      const gameSession = await GameSessions.findOne({
+        _id: new ObjectId(sessionId),
+      });
+
+      if (!gameSession)
+        throw new ApiError(404, "Session with that id does not exist");
+
+      // check if requester is an admin of the room by roomId
+      const userEmail = req.user.email;
+      const roomId = gameSession.roomId;
+      const room = await Rooms.findOne({ _id: new ObjectId(roomId) });
+
+      const roomAdminList = room!.adminList;
+      if (!roomAdminList.includes(userEmail))
+        throw new ApiError(403, "You are not an admin of this room");
+
+      // Update and return new document
+      const result = await GameSessions.findOneAndUpdate(
+        { _id: new ObjectId(sessionId) },
+        {
+          $set: {
+            start: new Date(req.body.start),
+            end: new Date(req.body.end),
+            cost: req.body.cost as number,
+            payTo: req.body.payTo,
+            courts: req.body.courts as string[],
+            title: req.body.title,
+          },
+        },
+        { returnDocument: "after" }
+      );
+      if (result.value === null)
+        throw new ApiError(404, "No session with that id");
+
+      res.status(200).json({ result: result.value });
+    } catch (error) {
+      next(error);
+    }
+  });
 
 export default router;
