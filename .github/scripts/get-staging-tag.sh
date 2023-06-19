32c9cbe534b5b27652dd68e7765d18f27375e460
PScoriae <pierreccesario@gmail.com>
2023-06-19 22:39:54 +0800

    fix(ci): increment rc logic simplified

--- .github/scripts/get-staging-tag.sh
+++ .github/scripts/get-staging-tag.sh
@@ -8,21 +8,23 @@ file_content=$(curl https://raw.githubusercontent.com/shuttleday/k8s/main/overla
 current_tag=$(awk -F':' '{print $3}' <<< "$file_content")
 
 # Extract semantic version of the tag
-current_semantic_version=$(echo "${current_tag}" | awk -F'-rc' '{print $1}')
+current_semantic_version=$(echo "${current_tag}" | awk -F'-rc' '{print $1}' | tr -d '\n')
 
 # Extract the numeric portion of "rc"
-current_rc_number=$(echo "${current_tag}" | awk -F'-rc' '{print $2}')
+current_rc_number=$(echo "${current_tag}" | awk -F'-rc' '{print $2}' | tr -d '\n')
 
-# Check if the input tag is different from the current tag
-if [[ "${input_tag}" != "${current_semantic_version}" ]]; then
-  updated_tag="${input_tag}-rc1"
-  echo "${updated_tag}"
-else
+# Check if the input tag is different from the current semantic tag
+if [ "$input_tag" = "$current_semantic_version" ]; then
+  echo "input tag is the same as the current semantic version"
   # Increment the "rc" number
   let updated_rc_number=$current_rc_number+1
   
   # Construct the updated tag with the incremented "rc" number
   updated_tag="${input_tag}-rc${updated_rc_number}"
   
+  echo "${updated_tag}"
+else
+  echo "input tag is different from the current semantic version"
+  updated_tag="${input_tag}-rc1"
   echo "${updated_tag}"
 fi
